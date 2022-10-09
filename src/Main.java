4a23ba9d007df5361eb56d05f83330c411c70261
Bogdan <ing.zahariucbogdan@gmail.com>
2022-10-09 21:02:53 +0300

    Tema 1 Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello");
+        System.out.println("Bogdan");
+        System.out.println(123 + 234);
+        System.out.println(75 / 3);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+
     }
+
 }
