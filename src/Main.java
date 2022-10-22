013bbb0344e6142943a1ec861a6bf8fbae51ec3a
Bogdan <ing.zahariucbogdan@gmail.com>
2022-10-22 14:05:24 +0300

    Tema Java

--- src/Main.java
+++ src/Main.java
@@ -104,33 +104,51 @@ public class Main {
 
 
     public static float sum(float first, float second) {
-        float result = first + second;
-        return result;
+        return first + second;
     }
     public static float substract(float first, float second){
-        float result = first - second;
-        return result;
+        return first - second;
     }
 
 
     public static float multiply(float first, float second){
-        float result = first * second;
-        return result;
+        return first * second;
     }
 
 
     public static float divide(float first, float second){
-        float result = first / second;
-        return result;
+        return first / second;
         }
 
 
     public static float avr(float a1, float b1, float c1) {
-        float result = (a1 + b1 + c1)/3;
-        return result;
+        return (a1 + b1 + c1)/3;
     }
 
+    public static class Main1 {
+        public static void main(String[] args) {
+            //if the text input is "FastTrack", print: "Learning comparison", if not, print :"Got to try more"
+            LogicalOperation op = new LogicalOperation();
+            String textInput = "FastTrack";
+            System.out.println(op.textComp(textInput));
 
+
+            // compare 2 numbers, get the greater number
+            int first = 5;
+            int second = 7;
+            System.out.println(op.getGreaterNumber(first, second));
+
+
+            //Given a text input and a number input,if the text is equal to "FastTrack" AND the number is
+            //equal to or lower than 3, print the number and the text, in this order. If the text is not "FastTrack"
+            //AND the number is equal to or higher than 4, print the number&text in this order.
+
+            String textInput2 = "FastTrack";
+            int numberInput = 5;
+
+            System.out.println(op.textAndNumberComparison(textInput2, numberInput));
+        }
+    }
 
 
 
