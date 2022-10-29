88f2cae9f28f057a7b7baf3ec08174b3172ff5e4
Bogdan <ing.zahariucbogdan@gmail.com>
2022-10-29 15:41:19 +0300

    Tema Java

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -3,27 +3,27 @@ package AnimalRescue;
 public class AppMain {
     public static void main(String[] args){
     Dog Rocky = new Dog();
-    Rocky.setname("Rocky");
-    Rocky.setage(2);
-    Rocky.setbreed("Bichon");
-    Rocky.setweight(2);
-    Rocky.setgender("Male");
-    Rocky.setsize("Small");
-    Rocky.setcolor("White");
-    Rocky.sethealth_level(8);
-    Rocky.setfavorite_food("Royal Canin");
-    Rocky.setrecreational_activity("walk in the Park");
+    Rocky.setName("Rocky");
+    Rocky.setAge(2);
+    Rocky.setBreed("Bichon");
+    Rocky.setWeight(7.6);
+    Rocky.setGender("Male");
+    Rocky.setSize("Small");
+    Rocky.setColor("White");
+    Rocky.setHealth_level(8);
+    Rocky.setFavorite_food("Royal Canin");
+    Rocky.setRecreational_activity("walk in the Park");
 
-        System.out.println("The dog name is: " + Rocky.getname());
-        System.out.println("The dog age is: " + Rocky.getage() + " years");
-        System.out.println("The dog weight is: " + Rocky.getweight() + " kg");
-        System.out.println("The dog size is: " + Rocky.getsize());
-        System.out.println("The dog breed is: " + Rocky.getbreed());
-        System.out.println("The dog gender is: " + Rocky.getgender());
-        System.out.println("The dog color is: " + Rocky.getcolor());
-        System.out.println("The dog level of health is: " + Rocky.gethealth_level());
-        System.out.println("The dog favorite food is: " + Rocky.getfavorite_food());
-        System.out.println("The dog favorite recreational activity is: " + Rocky.getrecreational_activity());
+        System.out.println("The dog name is: " + Rocky.getName());
+        System.out.println("The dog age is: " + Rocky.getAge() + " years");
+        System.out.println("The dog weight is: " + Rocky.getWeight() + " kg");
+        System.out.println("The dog size is: " + Rocky.getSize());
+        System.out.println("The dog breed is: " + Rocky.getBreed());
+        System.out.println("The dog gender is: " + Rocky.getGender());
+        System.out.println("The dog color is: " + Rocky.getColor());
+        System.out.println("The dog level of health is: " + Rocky.getHealth_level());
+        System.out.println("The dog favorite food is: " + Rocky.getFavorite_food());
+        System.out.println("The dog favorite recreational activity is: " + Rocky.getRecreational_activity());
 
 
     }
--- src/AnimalRescue/Dog.java
+++ src/AnimalRescue/Dog.java
@@ -4,7 +4,7 @@ public class Dog {
     private String name;
     private int age;
     private String breed;
-    private int weight;
+    private double weight;
     private String gender;
     private String size;
     private String color ;
@@ -13,67 +13,67 @@ public class Dog {
     private String recreational_activity ;
 
 
-    public void setname(String name){
+    public void setName(String name){
         this.name = name;
     }
-    public String getname() {
+    public String getName() {
         return name;
     }
-    public void setage(int age){
+    public void setAge(int age){
         this.age = age;
     }
-    public int getage() {
+    public int getAge() {
         return age;
     }
-    public void setbreed(String breed){
+    public void setBreed(String breed){
         this.breed = breed;
     }
-    public String getbreed() {
+    public String getBreed() {
         return breed;
     }
 
-    public void setweight(int weight){
+    public void setWeight(double weight){
         this.weight = weight;
     }
-    public int getweight() {
+    public double getWeight() {
         return weight;
     }
 
-    public void setgender(String gender){
+    public void setGender(String gender){
         this.gender = gender;
     }
-    public String getgender() {
+    public String getGender() {
         return gender;
     }
-    public void setsize(String size){
+    public void setSize(String size){
         this.size = size;
     }
-    public String getsize() {
+    public String getSize() {
         return size;
     }
-    public void setcolor(String color){
+    public void setColor(String color){
         this.color = color;
     }
-    public String getcolor() {
+    public String getColor() {
         return color;
     }
 
-    public void sethealth_level(int health_level){
+    public void setHealth_level(int health_level){
         this.health_level = health_level;
     }
-    public int gethealth_level() {
+    public int getHealth_level() {
         return health_level;
     }
-    public void setfavorite_food(String favorite_food){
+    public void setFavorite_food(String favorite_food){
         this.favorite_food = favorite_food;
     }
-    public String getfavorite_food() {
+    public String getFavorite_food() {
         return favorite_food;
     }
-    public void setrecreational_activity(String recreational_activity){
+    public void setRecreational_activity(String recreational_activity){
         this.recreational_activity = recreational_activity;
     }
-    public String getrecreational_activity() {
+    public String getRecreational_activity() {
         return recreational_activity;
     }
 
